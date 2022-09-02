ae72defed3ec8cd5342def8e1bf14074edca0b70
hokita <hideee.0202@gmail.com>
2022-09-02 13:06:15 +0900

    Add smartphone window setting to new payment page.

--- front/pages/payments/new.tsx
+++ front/pages/payments/new.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import { Heading, Button, Box } from '@chakra-ui/react'
 import { NewPaymentForm } from '../../features/payments/Payments'
 import { useState, useEffect } from 'react'
@@ -7,20 +8,29 @@ import { createPayment } from '../../features/payments/paymentsSlice'
 
 const New = () => {
   return (
-    <Box m={5}>
-      <Heading as="h1" size="xl" mb={3}>
-        Kingyo
-      </Heading>
-      <Heading as="h2" size="lg" mb={3}>
-        Create New Payment
-      </Heading>
-      <NewPaymentForm />
-      <Box>
-        <Link href="/">
-          <a>cancel</a>
-        </Link>
+    <>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+      </Head>
+      <Box m={5}>
+        <Heading as="h1" size="xl" mb={3}>
+          Kingyo
+        </Heading>
+        <Heading as="h2" size="lg" mb={3}>
+          Create New Payment
+        </Heading>
+        <NewPaymentForm />
+        <Box>
+          <Link href="/">
+            <a>cancel</a>
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
 
