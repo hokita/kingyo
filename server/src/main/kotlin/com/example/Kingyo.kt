5b752a5df070b3819dd4f94638ad528a7b8a0fd6
hokita <hideee.0202@gmail.com>
2022-10-10 22:08:55 +0900

    Change DB.

--- server/src/main/kotlin/com/example/Kingyo.kt
+++ server/src/main/kotlin/com/example/Kingyo.kt
@@ -26,9 +26,9 @@ fun main() {
     val connection: Connection
     try {
         connection = DriverManager.getConnection(
-            "jdbc:mysql://db/kingyo?autoReconnect=true",
-            "sa",
-            "1234"
+            "jdbc:mysql://localhost/kingyo?autoReconnect=true",
+            "kingyo",
+            "password"
         )
     } catch (e: Exception) {
         e.printStackTrace()
